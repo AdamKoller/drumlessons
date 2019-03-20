ae7fdd86a97ca1ac6b9f3a470cafeb5f3e9cdea8
Adam Koller <41529662+AdamKoller@users.noreply.github.com>
2019-03-19 19:27:22 -0500

    created object

--- drumlessons.js
+++ drumlessons.js
@@ -2,44 +2,18 @@
   var lesson = {
     type : '1-1 Private',
     lessonRate : 50,
-    discount : 15,
+    discount : 30,
     offerPrice : function () {
       var offerRate = this.lessonRate * ((100 - this.discount) / 100);
+      return offerRate + '/hr';
     }
   };
 
-//   var lessonType,lessonRate,specialRate;
-//   lessonType = document.getElementById('lessonType');
-//   lessonRate = document.getElementById('lessonRate');
-//   specialRate = document.getElementById('specialRate');
-//
-//   lessonType.textContent = lesson.type;
-//   lessonRate.textContent = '$' + lesson.lessonRate.toFixed(2);
-//   specialRate.textContent = '$' + lesson.offerPrice();
-//
-//   var expireMsg;
-//   var today;
-//   var elEnds;
-//
-//   function offerExpires(today) {
-//     var weekFromToday, day, date, month, year, dayNames, monthNames;
-//
-//     weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-//     dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//     monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-//
-//     day = dayNames[weekFromToday.getDay()];
-//     date = weekFromToday.getDate();
-//     month = monthNames[weekFromToday.getMonth()];
-//     year = weekFromToday.getFullYear();
-//
-//     expireMsg = 'Offer Expires Next ';
-//     expireMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
-//     return expireMsg;
-//   }
-//
-//   today = new Date();
-//   elEnds = document.getElementById('offerEnds');
-//   elEnds.innerHTML = offerExpires(today);
-//
-// }());
+  var lessonType,lessonRate,specialRate;
+  lessonType = document.getElementById('lessonType');
+  lessonRate = document.getElementById('lessonRate');
+  specialRate = document.getElementById('specialRate');
+
+  lessonType.textContent = lesson.type;
+  lessonRate.textContent = '$' + lesson.lessonRate.toFixed(2);
+  specialRate.textContent = '$' + lesson.offerPrice();
